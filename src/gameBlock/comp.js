bd174eea5f0241b87de8347280ab99c58a7fa082
ThePiroman <thepiroman1337@gmail.com>
2025-09-09 21:20:15 +0300

    feat: basic gameplay loop

--- src/gameBlock/comp.js
+++ src/gameBlock/comp.js
@@ -1,4 +1,5 @@
 import './style.css'
+import { checkWinCondition, doWin, won, debug, debugMineColor} from '../main.js'
 
 export default class gameBlock {
     constructor(parent) {
@@ -9,6 +10,10 @@ export default class gameBlock {
         const comp = document.createElement('div');
         comp.classList.add('div');
         comp.className = 'gameBlock';
+
+        comp.opened = false;
+        comp.flagged = false;
+        comp.mined = false;
 
         let compWidth = Number.parseInt(document.getElementsByClassName("gameCanvas")[0].style.width, 10)
         let compHeight = Number.parseInt(document.getElementsByClassName("gameCanvas")[0].style.height, 10)
@@ -24,8 +29,75 @@ export default class gameBlock {
         comp.style.width = 32 - (Number.parseInt(comp.style.borderWidth, 10) * 2) + 'px';
 
         comp.addEventListener("click", function(event) {
-            console.log("kruto")
+            if (won) {
+                return;
+            }
+
+            if (comp.flagged) {
+                new Audio("assets/sound/cant_click.mp3").play();
+                return;
+            }
+
+            if (comp.opened) {
+                return;
+            }
+
+            if (comp.mined) {
+
+                comp.style.backgroundColor = "red";
+
+                new Audio("assets/sound/boom.mp3").play();
+
+                setTimeout(function() {
+                    location.reload();
+                }, 1000)
+
+            } else {
+
+                comp.style.visibility = "hidden";
+
+            }
+
+            comp.opened = true;
+
+            if (checkWinCondition()) {
+                doWin()
+            }
+
+            
         });
+
+        comp.addEventListener("contextmenu", function(event) {
+            event.preventDefault();
+
+            if (won) {
+                return;
+            }
+            
+
+            if (!comp.flagged) {
+
+                comp.flagged = true;
+
+                comp.style.backgroundColor = "yellow";
+
+            } else {
+
+                comp.flagged = false;
+
+                comp.style.backgroundColor = "black";
+
+                if (debug && comp.mined) {
+                    comp.style.backgroundColor = debugMineColor;
+                }
+
+            }
+
+            new Audio("assets/sound/flag_place.mp3").play();
+
+        });
+
+
 
         return comp;
     }
--- src/main.js
+++ src/main.js
@@ -1,20 +1,79 @@
-import './reset.css'
-import './normalize.css'
-import gameCanvas from './gameCanvas/comp'
-import gameBlock from './gameBlock/comp'
+import './reset.css';
+import './normalize.css';
+import gameCanvas from './gameCanvas/comp';
+import gameBlock from './gameBlock/comp';
+
+export var won = false;
+export var debug = false;
+export var debugMineColor = "gray";
 
 
-const app = document.querySelector('#app')
+const app = document.querySelector('#app');
 
-new gameCanvas(app, 256).render()
+new gameCanvas(app, 256).render();
 
-new gameBlock(document.getElementsByClassName("gameCanvas")[0]).render()
-new gameBlock(document.getElementsByClassName("gameCanvas")[0]).render()
-new gameBlock(document.getElementsByClassName("gameCanvas")[0]).render()
-new gameBlock(document.getElementsByClassName("gameCanvas")[0]).render()
-new gameBlock(document.getElementsByClassName("gameCanvas")[0]).render()
-new gameBlock(document.getElementsByClassName("gameCanvas")[0]).render()
-new gameBlock(document.getElementsByClassName("gameCanvas")[0]).render()
-new gameBlock(document.getElementsByClassName("gameCanvas")[0]).render()
+function createRow() {
+    for (let i = 1; i < 9; i++) {
+        new gameBlock(document.getElementsByClassName("gameCanvas")[0]).render();
+    }
+}
+
+function randomizeMines() {
+    let minesAmount = 10;
+
+    while (minesAmount >= 0) {
+       Array.prototype.forEach.call(document.getElementsByClassName("gameBlock"), block => {
+            if (minesAmount < 0) {
+                return;
+            }
+            if (Math.random() < 0.25) {
+                block.mined = true;
+
+                if (debug) {
+                    block.style.backgroundColor = debugMineColor;
+                }
+                
+                minesAmount -= 1;
+            }
+        });
+    }
+
+}
+
+export function checkWinCondition() {
+    let returnValue = true;
+    Array.prototype.forEach.call(document.getElementsByClassName("gameBlock"), block => {
+        if (!block.opened) {
+            if (!block.mined) {
+                returnValue = false;
+            }
+        }
+
+    });
+
+    return returnValue;
+
+
+}
+
+export function doWin() {
+    Array.prototype.forEach.call(document.getElementsByClassName("gameBlock"), block => {
+
+        block.style.backgroundColor = "red";
+
+    });
+
+    new Audio("assets/sound/win.mp3").play();
+
+    setTimeout(function() {location.reload()}, 6000)
+}
+
+
+
+for (let i = 1; i < 8; i++) {
+    createRow();
+}
+
+randomizeMines()
 
 
